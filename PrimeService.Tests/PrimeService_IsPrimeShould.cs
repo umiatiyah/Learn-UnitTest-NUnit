68069a74047f921b05b2262f7b953efc39232c7f
umiatiyah <umiatiyah97@gmail.com>
2022-08-05 23:57:30 +0700

    implement nunit test read prime number

--- PrimeService.Tests/PrimeService_IsPrimeShould.cs
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -1,15 +1,25 @@
-namespace PrimeService.Tests;
+using NUnit.Framework;
+using Prime.Services;
 
-public class Tests
+namespace Prime.UnitTests.Services
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
     {
-    }
+        private PrimeService _primeService;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [SetUp]
+        public void SetUp()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Test]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
     }
 }
